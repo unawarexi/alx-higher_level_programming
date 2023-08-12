ea0160b3068bfeb3326211aa960e24bf7459bd3c
unawarexi <unawarexi@gmail.com>
2023-08-11 19:39:52 -0700

    import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 
 if __name__ == "__main__":
+    """mathematical basic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-    if len(sys.argv) != 4:
-        print("mode: ./100-my_calculator.py <a> <operator> <b>")
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in "+-*/":
-        print("Unfamiliar operator detected. Operators youll use include +, -, *, and /.")
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unfamiliar operator detected. Operators youll use include +, -, *, and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
 
-    print(f"{a} {sys.argv[2]} {b} = ", end="")
-    if sys.argv[2] == '+':
-        print(f"{add(a, b)}")
-    elif sys.argv[2] == '-':
-        print(f"{sub(a, b)}")
-    elif sys.argv[2] == '*':
-        print(f"{mul(a, b)}")
-    elif sys.argv[2] == '/':
-        print(f"{div(a, b)}")
